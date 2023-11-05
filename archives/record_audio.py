0f97f6df012b6b6e08ac86509352955c2da572a0
Dhravya <dhravyashah@gmail.com>
2023-11-05 00:05:30 -0700

    update requirements.txt

--- archives/record_audio.py
+++ archives/record_audio.py
@@ -1,44 +1,44 @@
-import pyaudio
-import wave
+# import pyaudio
+# import wave
 
-def record_audio():
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 44100
-    CHUNK = 1024
-    RECORD_SECONDS = 10
-    OUTPUT_FILENAME = "recording.wav"
+# def record_audio():
+#     FORMAT = pyaudio.paInt16
+#     CHANNELS = 1
+#     RATE = 44100
+#     CHUNK = 1024
+#     RECORD_SECONDS = 10
+#     OUTPUT_FILENAME = "recording.wav"
 
-    audio = pyaudio.PyAudio()
+#     audio = pyaudio.PyAudio()
 
-    # Start recording
-    stream = audio.open(
-        format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK
-    )
-    print("Recording...")
+#     # Start recording
+#     stream = audio.open(
+#         format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK
+#     )
+#     print("Recording...")
 
-    frames = []
+#     frames = []
 
-    for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
+#     for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+#         data = stream.read(CHUNK)
+#         frames.append(data)
 
-    print("Finished recording.")
+#     print("Finished recording.")
 
-    # Stop recording
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
+#     # Stop recording
+#     stream.stop_stream()
+#     stream.close()
+#     audio.terminate()
 
-    # Save the recording as a WAV file
-    with wave.open(OUTPUT_FILENAME, "wb") as wf:
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(audio.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b"".join(frames))
+#     # Save the recording as a WAV file
+#     with wave.open(OUTPUT_FILENAME, "wb") as wf:
+#         wf.setnchannels(CHANNELS)
+#         wf.setsampwidth(audio.get_sample_size(FORMAT))
+#         wf.setframerate(RATE)
+#         wf.writeframes(b"".join(frames))
 
-    print(f"Recording saved to {OUTPUT_FILENAME}")
-    return True
+#     print(f"Recording saved to {OUTPUT_FILENAME}")
+#     return True
 
-if __name__ == "__main__":
-    record_audio()
+# if __name__ == "__main__":
+#     record_audio()
